13637d634df3757062df6fc9768c011e8a4f4bb0
Yael20026 <hiram060220@gmail.com>
2025-09-19 21:22:22 -0600

    pruebas lay out flask

--- app.py
+++ app.py
@@ -5,15 +5,13 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for, f
 app = Flask(__name__)
 app.secret_key = "super_secret_key"  # Cambia esto por algo más seguro
 
-# Simulación de usuarios registrados (puedes cambiarlo por una base de datos)
-usuarios = {
-    "admin": "1234",
-    "hiram": "pass123"
-}
-
 @app.route("/")
 def index():
-    return render_template("index.html")  # Renderiza el login
+    return render_template("index.html")   
+
+@app.route('/house')
+def home():
+    return render_template('house.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
